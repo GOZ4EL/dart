727da30b810dc7555c9251b37a4f75e626183a1e
goz4el <hordemzerado@gmail.com>
2021-07-20 17:17:40 -0400

    using await for with Streams

--- console-full/bin/console_full.dart
+++ console-full/bin/console_full.dart
@@ -3,23 +3,33 @@ import 'dart:async';
 void main(List<String> arguments) async {
 //  loadString("Hello from dart").then((s) => print(s));
 
-  var str = await loadString("Hello from dart async");
-  print(str);
+//  var str = await loadString("Hello from dart async");
+//  print(str);
 
 
-  var x = await addAsync(59);
-  var z = await x(40);
-  print(z);
+//  var x = await addAsync(59);
+//  var z = await x(40);
+//  print(z);
+
+  Stream stream = exampleStream(20);
+  await for(var s in stream) {
+    print("Number: $s");
+  }
 }
 
-Future<String> loadString(String str) async {
-  await Future.delayed(Duration(seconds: 1));
-  return "String: $str";
-}
+Stream<int> exampleStream(int x) async* {
+  for(var i = 0; i <= x; i++) {
+    yield i;
+  }
+}  
+//Future<String> loadString(String str) async {
+//  await Future.delayed(Duration(seconds: 1));
+//  return "String: $str";
+//}
 
-Future<Function> addAsync(int x) async {
-  return (int y) async => x + y;
-}
+//Future<Function> addAsync(int x) async {
+//  return (int y) async => x + y;
+//}
 
 //Future<String> loadString(String str) {
 //  return Future.delayed(Duration(seconds: 1)).then((_) {
