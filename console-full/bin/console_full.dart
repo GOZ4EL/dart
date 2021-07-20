e54524008a4842e0efddcd3bb9a09122051c9d4f
goz4el <hordemzerado@gmail.com>
2021-07-20 16:08:06 -0400

    using async await

--- console-full/bin/console_full.dart
+++ console-full/bin/console_full.dart
@@ -1,11 +1,18 @@
 import 'dart:async';
 
-void main(List<String> arguments) {
-  loadString("Hello from dart").then((s) => print(s));
+void main(List<String> arguments) async {
+//  loadString("Hello from dart").then((s) => print(s));
+  String str = await loadString("Hello from dart async");
+  print(str);
 }
 
-Future<String> loadString(String str) {
-  return Future.delayed(Duration(seconds: 1)).then((_) {
-    return "String: $str";
-  });
+Future<String> loadString(String str) async {
+  await Future.delayed(Duration(seconds: 1));
+  return "String: $str";
 }
+
+//Future<String> loadString(String str) {
+//  return Future.delayed(Duration(seconds: 1)).then((_) {
+//    return "String: $str";
+//  });
+//}
