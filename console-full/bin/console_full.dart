3ba3ad807c6a0b80b11ade39244d86f52b9bfdd6
goz4el <hordemzerado@gmail.com>
2021-07-20 16:34:42 -0400

    using Futures a little bit more complicateds

--- console-full/bin/console_full.dart
+++ console-full/bin/console_full.dart
@@ -2,13 +2,23 @@ import 'dart:async';
 
 void main(List<String> arguments) async {
 //  loadString("Hello from dart").then((s) => print(s));
-  String str = await loadString("Hello from dart async");
+
+  var str = await loadString("Hello from dart async");
   print(str);
+
+
+  var x = await addAsync(59);
+  var z = await x(40);
+  print(z);
 }
 
 Future<String> loadString(String str) async {
   await Future.delayed(Duration(seconds: 1));
   return "String: $str";
+}
+
+Future<Function> addAsync(int x) async {
+  return (int y) async => x + y;
 }
 
 //Future<String> loadString(String str) {
