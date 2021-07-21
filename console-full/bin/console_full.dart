c330fdfe8b59c8aa7bfc85206782cb278260dd68
goz4el <hordemzerado@gmail.com>
2021-07-20 21:50:34 -0400

    Using streams for writing and reading on files

--- console-full/bin/console_full.dart
+++ console-full/bin/console_full.dart
@@ -1,4 +1,6 @@
 import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
 
 void main(List<String> arguments) async {
 //  loadString("Hello from dart").then((s) => print(s));
@@ -15,14 +17,35 @@ void main(List<String> arguments) async {
 //  await for(var s in stream) {
 //    print("Number: $s");
 //  }
-  exampleStream(20).listen((int x) => print("Number: $x"));
+
+//  exampleStream(20).listen((int x) => print("Number: $x"));
+
+  var file = File("text.txt");
+  var sink = file.openWrite(mode: FileMode.append);
+  sink.add(utf8.encode("Hello from dart at ${DateTime.now().toIso8601String()}\n"));
+
+  await sink.flush();
+  await sink.close();
+
+  var fileStream = file.openRead().asBroadcastStream();
+
+  fileStream
+    .transform(utf8.decoder)
+    .transform(LineSplitter())
+    .listen((str) => print(str),
+      onDone: () => print("Done reading file"),
+      onError: (e) => print(e.toString())
+    )
+  ;
+
+  fileStream.listen((byte) => print(byte)); 
 }
 
-Stream<int> exampleStream(int x) async* {
-  for(var i = 0; i <= x; i++) {
-    yield i;
-  }
-}  
+//Stream<int> exampleStream(int x) async* {
+//  for(var i = 0; i <= x; i++) {
+//    yield i;
+//  }
+//}  
 //Future<String> loadString(String str) async {
 //  await Future.delayed(Duration(seconds: 1));
 //  return "String: $str";
