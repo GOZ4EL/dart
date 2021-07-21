4962e52763aaa44e54446e7d7e507374d3e99c3a
goz4el <hordemzerado@gmail.com>
2021-07-21 09:22:18 -0400

    using objects, final attributes, static classes and override.

--- oop/bin/oop.dart
+++ oop/bin/oop.dart
@@ -1,5 +1,34 @@
-import 'package:oop/oop.dart' as oop;
-
 void main(List<String> arguments) {
-  print('Hello world: ${oop.calculate()}!');
+  var point = Point.zero()
+    ..x = 33
+    ..y = 45;
+  
+  print(point.toString());
+}
+
+class Point{
+  final String name;
+  int x = 0;
+  int y = 0;
+
+  static final Map<String, Point> _cache = <String, Point>{};
+
+  Point(this.x, this.y, {this.name = 'zero'});
+  
+  Point.zero() : name = 'zero';
+
+
+  void save() {
+    _cache[name] = Point(x, y, name: name);
+  }
+
+  int addTogether(){
+    return x + y;
+  }
+
+  @override
+  String toString() => 'Point at ($x, $y) named: $name';
+
+  @override
+  int get hashCode => super.hashCode;
 }
